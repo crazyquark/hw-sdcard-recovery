f2f0c35c77c9705d4c75946aa4d3e6d5fc25d705
crazyquark <cristian.sandu@gmail.com>
2018-06-08 08:21:21 +0300

    LCD stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <AStar32U4.h>
 #include <SdFs.h>
 
 /*
@@ -32,6 +33,14 @@ const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
 #endif // HAS_SDIO_CLASS
 
 //------------------------------------------------------------------------------
+AStar32U4LCD lcd;
+AStar32U4Buzzer buzzer;
+AStar32U4ButtonA buttonA;
+AStar32U4ButtonB buttonB;
+AStar32U4ButtonC buttonC;
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
 SdFs sd;
 cid_t m_cid;
 csd_t m_csd;
@@ -39,6 +48,17 @@ uint32_t m_eraseSize;
 uint32_t m_ocr;
 static ArduinoOutStream cout(Serial);
 //------------------------------------------------------------------------------
+
+void errorPrint()
+{
+    if (sd.sdErrorCode())
+    {
+        cout << F("SD errorCode: ") << hex << showbase;
+        printSdErrorSymbol(&Serial, sd.sdErrorCode());
+        cout << F(" = ") << int(sd.sdErrorCode()) << endl;
+        cout << F("SD errorData = ") << int(sd.sdErrorData()) << endl;
+    }
+}
 
 void printConfig(SdSpiConfig config)
 {
@@ -59,8 +79,103 @@ void printConfig(SdSpiConfig config)
     cout << F("\nEdit SD_CS_PIN to change the SD chip select pin.\n");
 }
 
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+bool cidDmp()
+{
+    cout << F("\nManufacturer ID: ");
+    cout << uppercase << showbase << hex << int(m_cid.mid) << dec << endl;
+    cout << F("OEM ID: ") << m_cid.oid[0] << m_cid.oid[1] << endl;
+    cout << F("Product: ");
+    for (uint8_t i = 0; i < 5; i++)
+    {
+        cout << m_cid.pnm[i];
+    }
+    cout << F("\nVersion: ");
+    cout << int(m_cid.prv_n) << '.' << int(m_cid.prv_m) << endl;
+    cout << F("Serial number: ") << hex << m_cid.psn << dec << endl;
+    cout << F("Manufacturing date: ");
+    cout << int(m_cid.mdt_month) << '/';
+    cout << (2000 + m_cid.mdt_year_low + 10 * m_cid.mdt_year_high) << endl;
+    cout << endl;
+    return true;
+}
+
+//------------------------------------------------------------------------------
+bool csdDmp()
+{
+    bool eraseSingleBlock;
+    if (m_csd.v1.csd_ver == 0)
+    {
+        eraseSingleBlock = m_csd.v1.erase_blk_en;
+        m_eraseSize = (m_csd.v1.sector_size_high << 1) | m_csd.v1.sector_size_low;
+    }
+    else if (m_csd.v2.csd_ver == 1)
+    {
+        eraseSingleBlock = m_csd.v2.erase_blk_en;
+        m_eraseSize = (m_csd.v2.sector_size_high << 1) | m_csd.v2.sector_size_low;
+    }
+    else
+    {
+        cout << F("m_csd version error\n");
+        return false;
+    }
+    m_eraseSize++;
+    cout << F("cardSize: ") << 0.000512 * sdCardCapacity(&m_csd);
+    cout << F(" MB (MB = 1,000,000 bytes)\n");
+
+    cout << F("flashEraseSize: ") << int(m_eraseSize) << F(" blocks\n");
+    cout << F("eraseSingleBlock: ");
+    if (eraseSingleBlock)
+    {
+        cout << F("true\n");
+    }
+    else
+    {
+        cout << F("false\n");
+    }
+    return true;
+}
+//------------------------------------------------------------------------------
+
+void printCardType()
+{
+
+    cout << F("\nCard type: ");
+
+    switch (sd.card()->type())
+    {
+    case SD_CARD_TYPE_SD1:
+        cout << F("SD1\n");
+        break;
+
+    case SD_CARD_TYPE_SD2:
+        cout << F("SD2\n");
+        break;
+
+    case SD_CARD_TYPE_SDHC:
+        if (sdCardCapacity(&m_csd) < 70000000)
+        {
+            cout << F("SDHC\n");
+        }
+        else
+        {
+            cout << F("SDXC\n");
+        }
+        break;
+
+    default:
+        cout << F("Unknown\n");
+    }
+}
+//------------------------------------------------------------------------------
+
 void setup()
 {
+    lcd.clear();
+    lcd.print("Init...");
+
     Serial.begin(9600);
     // Wait for USB Serial
     while (!Serial)
@@ -68,9 +183,68 @@ void setup()
         SysCall::yield();
     }
     printConfig(SD_CONFIG);
+
+    lcd.clear();
+    lcd.print("Serial");
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    // Read any existing Serial data.
+    do
+    {
+        delay(10);
+    } while (Serial.available() && Serial.read() >= 0);
+
+    // F stores strings in flash to save RAM
+    cout << F("\ntype any character to start\n");
+    while (!Serial.available())
+    {
+        SysCall::yield();
+    }
+    uint32_t t = millis();
+
+    lcd.clear();
+    lcd.print("SD nfo");
+
+    if (!sd.cardBegin(SD_CONFIG))
+    {
+        cout << F(
+            "\nSD initialization failed.\n"
+            "Do not reformat the card!\n"
+            "Is the card correctly inserted?\n"
+            "Is there a wiring/soldering problem?\n");
+        if (isSpi(SD_CONFIG))
+        {
+            cout << F(
+                "Is SD_CS_PIN set to the correct value?\n"
+                "Does another SPI device need to be disabled?\n");
+        }
+        errorPrint();
+        return;
+    }
+    t = millis() - t;
+    cout << F("init time: ") << t << " ms" << endl;
+
+    if (!sd.card()->readCID(&m_cid) ||
+        !sd.card()->readCSD(&m_csd) ||
+        !sd.card()->readOCR(&m_ocr))
+    {
+        cout << F("readInfo failed\n");
+        errorPrint();
+        return;
+    }
+    printCardType();
+    cidDmp();
+    csdDmp();
+    cout << F("\nOCR: ") << uppercase << showbase;
+    cout << hex << m_ocr << dec << endl;
+    // mbrDmp();
+    // if (!sd.volumeBegin())
+    // {
+    //     cout << F("\nvolumeBegin failed. Is the card formatted?\n");
+    //     errorPrint();
+    //     return;
+    // }
+    // dmpVol();
 }
